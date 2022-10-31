bae5d6cf9ae62b5557d12fa96fda86ef11ec97c7
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-10-31 14:42:36 -0700

    Switch to opam packaging

---
+++ install.cmd
@@ -0,0 +1,19 @@
+SETLOCAL ENABLEEXTENSIONS
+
+@ECHO ---------------------
+@ECHO Arguments:
+@ECHO   Target directory = %1
+@ECHO ---------------------
+SET TARGETDIR=%1
+
+@REM Create unix/ subdirectory including any parent directories (extensions are enabled)
+MKDIR %TARGETDIR%\unix
+
+@REM Create all/emptytop/ subdirectory including any parent directories (extensions are enabled)
+MKDIR %TARGETDIR%\all\emptytop
+
+@REM Copy in binary mode so that CRLF is not added
+COPY /Y /B unix\_common_tool.sh             %TARGETDIR%\unix
+COPY /Y /B unix\_within_dev.sh              %TARGETDIR%\unix
+COPY /Y /B unix\crossplatform-functions.sh  %TARGETDIR%\unix
+COPY /Y /B all\emptytop\dune-project        %TARGETDIR%\all\emptytop
---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+targetdir=$1
+shift
+
+echo -- ---------------------
+echo Arguments:
+echo "  Target directory = $targetdir"
+echo -- ---------------------
+
+install -d "$targetdir/unix" "$targetdir/all/emptytop"
+
+install unix/_common_tool.sh unix/_within_dev.sh unix/crossplatform-functions.sh "$targetdir/unix/"
+install all/emptytop/dune-project "$targetdir/all/emptytop/"

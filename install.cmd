6f23161b82e6c168697490fae2eee11caf1ec297
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-12-28 02:35:51 -0800

    Drop leading dot to avoid findlib and copy problems

--- install.cmd
+++ install.cmd
@@ -14,11 +14,14 @@ MKDIR %TARGETDIR%\all\emptytop
 
 @REM Copy in binary mode so that CRLF is not added
 XCOPY /Y /H /N /I META                             %TARGETDIR%
-@REM   Since .template.dkmlroot goes into library, we drop the leading dot to avoid any future findlib problems
-XCOPY /Y /H /N /I .template.dkmlroot               %TARGETDIR%
-IF EXIST %TARGETDIR%\template.dkmlroot DEL   /F /Q %TARGETDIR%\template.dkmlroot 
-REN   %TARGETDIR%\.template.dkmlroot               template.dkmlroot 
+IF %ERRORLEVEL% NEQ 0 (Echo Error during XCOPY &Exit /b 1)
+XCOPY /Y /H /N /I template.dkmlroot                %TARGETDIR%
+IF %ERRORLEVEL% NEQ 0 (Echo Error during XCOPY &Exit /b 1)
 XCOPY /Y /H /N /I unix\_common_tool.sh             %TARGETDIR%\unix
+IF %ERRORLEVEL% NEQ 0 (Echo Error during XCOPY &Exit /b 1)
 XCOPY /Y /H /N /I unix\_within_dev.sh              %TARGETDIR%\unix
+IF %ERRORLEVEL% NEQ 0 (Echo Error during XCOPY &Exit /b 1)
 XCOPY /Y /H /N /I unix\crossplatform-functions.sh  %TARGETDIR%\unix
+IF %ERRORLEVEL% NEQ 0 (Echo Error during XCOPY &Exit /b 1)
 XCOPY /Y /H /N /I all\emptytop\dune-project        %TARGETDIR%\all\emptytop
+IF %ERRORLEVEL% NEQ 0 (Echo Error during XCOPY &Exit /b 1)
--- install.sh
+++ install.sh
@@ -11,7 +11,6 @@ echo -- ---------------------
 install -d "$targetdir/unix" "$targetdir/all/emptytop"
 
 install META "$targetdir/"
-#       Since .template.dkmlroot goes into library, we drop the leading dot to avoid any future findlib problems
-install .template.dkmlroot "$targetdir/template.dkmlroot"
+install template.dkmlroot "$targetdir/"
 install unix/_common_tool.sh unix/_within_dev.sh unix/crossplatform-functions.sh "$targetdir/unix/"
 install all/emptytop/dune-project "$targetdir/all/emptytop/"

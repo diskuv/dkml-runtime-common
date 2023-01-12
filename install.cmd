351344c850e9a515798031ddac3b9c7d496f6779
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2023-01-11 17:25:21 -0800

    Put double-quotes around win32 target dir

--- install.cmd
+++ install.cmd
@@ -7,21 +7,21 @@ SETLOCAL ENABLEEXTENSIONS
 SET TARGETDIR=%1
 
 @REM Create unix/ subdirectory including any parent directories (extensions are enabled)
-MKDIR %TARGETDIR%\unix
+MKDIR "%TARGETDIR%\unix"
 
 @REM Create all/emptytop/ subdirectory including any parent directories (extensions are enabled)
-MKDIR %TARGETDIR%\all\emptytop
+MKDIR "%TARGETDIR%\all\emptytop"
 
 @REM Copy in binary mode so that CRLF is not added
-XCOPY /Y /H /I META                             %TARGETDIR%
+XCOPY /Y /H /I META                             "%TARGETDIR%"
 IF %ERRORLEVEL% NEQ 0 (Echo Error during XCOPY &Exit /b 1)
-XCOPY /Y /H /I template.dkmlroot                %TARGETDIR%
+XCOPY /Y /H /I template.dkmlroot                "%TARGETDIR%"
 IF %ERRORLEVEL% NEQ 0 (Echo Error during XCOPY &Exit /b 1)
-XCOPY /Y /H /I unix\_common_tool.sh             %TARGETDIR%\unix
+XCOPY /Y /H /I unix\_common_tool.sh             "%TARGETDIR%\unix"
 IF %ERRORLEVEL% NEQ 0 (Echo Error during XCOPY &Exit /b 1)
-XCOPY /Y /H /I unix\_within_dev.sh              %TARGETDIR%\unix
+XCOPY /Y /H /I unix\_within_dev.sh              "%TARGETDIR%\unix"
 IF %ERRORLEVEL% NEQ 0 (Echo Error during XCOPY &Exit /b 1)
-XCOPY /Y /H /I unix\crossplatform-functions.sh  %TARGETDIR%\unix
+XCOPY /Y /H /I unix\crossplatform-functions.sh  "%TARGETDIR%\unix"
 IF %ERRORLEVEL% NEQ 0 (Echo Error during XCOPY &Exit /b 1)
-XCOPY /Y /H /I all\emptytop\dune-project        %TARGETDIR%\all\emptytop
+XCOPY /Y /H /I all\emptytop\dune-project        "%TARGETDIR%\all\emptytop"
 IF %ERRORLEVEL% NEQ 0 (Echo Error during XCOPY &Exit /b 1)

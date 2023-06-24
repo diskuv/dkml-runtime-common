b07cbad403bc67a8fa1e4385fe62d4801929be77
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2023-06-24 07:40:50 -0700

    Local, semver compliant version bumping

---
+++ bump-version.cmake
@@ -0,0 +1,14 @@
+# Test with:
+# cmake --log-context -D DRYRUN=1 -D CMAKE_MESSAGE_CONTEXT=dkml-runtime-common -D DKML_VERSION_SEMVER=1.2.1-2 -D DKML_VERSION_OPAMVER=1.2.1~prerel2 -D DKML_VERSION_SEMVER_NEW=1.2.1-3 -D DKML_VERSION_OPAMVER_NEW=1.2.1~prerel3 -D GIT_EXECUTABLE=git -D DKML_RELEASE_OCAML_VERSION=4.14.0 -D DKML_RELEASE_PARTICIPANT_MODULE=../../../packaging/version-bump/DkMLReleaseParticipant.cmake -P bump-version.cmake
+
+if(NOT DKML_RELEASE_PARTICIPANT_MODULE)
+    message(FATAL_ERROR "Missing -D DKML_RELEASE_PARTICIPANT_MODULE=.../DkMLReleaseParticipant.cmake")
+endif()
+include(${DKML_RELEASE_PARTICIPANT_MODULE})
+
+DkMLReleaseParticipant_PlainReplace(template.dkmlroot)
+DkMLReleaseParticipant_PlainReplace(README.md)
+DkMLReleaseParticipant_MetaReplace(META)
+DkMLReleaseParticipant_OpamReplace(dkml-runtime-common.opam)
+DkMLReleaseParticipant_OpamReplace(dkml-runtime-common-native.opam)
+DkMLReleaseParticipant_GitAddAndCommit()
